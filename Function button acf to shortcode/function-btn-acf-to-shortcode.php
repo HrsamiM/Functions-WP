b02b8e97de1ceb6165c58aa3ce91afee359f9794
Herson Samir Martinez <93807097+HrsamiM@users.noreply.github.com>
2021-12-14 22:48:35 -0600

    Update function-btn-acf-to-shortcode.php

--- Function button acf to shortcode/function-btn-acf-to-shortcode.php
+++ Function button acf to shortcode/function-btn-acf-to-shortcode.php
@@ -1,8 +1,8 @@
 <?php
 //Function LNK descargar manual ACF button
 function manual_button_on_product_page() {
-  $manual = get_field('pdf_download');
- echo '<a href='.$manual.'> <button type="button" class="btn-descargas">Descargar Manual</button></a>';
+	$manual = get_field('pdf_download');
+	echo '<a href="'.esc_url($manual).'" target="_blank"> <button type="button" class="hrm-btnCPT">Descargar Manual</button></a>';
 }
 add_shortcode( 'btn_manual_shortc', 'manual_button_on_product_page' );
 ?>
