2e1c85bb3852d154f8cd5a61099a6f07737e0856
Hr <93807097+HrsamiM@users.noreply.github.com>
2024-02-21 16:46:20 -0600

    Update and rename Custom CSS Function to custom-css-function.php

--- custom-css-function.php
+++ custom-css-function.php
@@ -1,6 +1,8 @@
+<?php
 //Add My Style File CSS
 function my_style_template(){
     wp_enqueue_style( 'style-template', get_stylesheet_directory_uri() . '/assets/css/style-template.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_style_template',5 );
 //End MyStyle Function
+?>
