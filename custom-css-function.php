04b6df29653b48c0eb3c04400eca90bcda93372e
Hr <93807097+HrsamiM@users.noreply.github.com>
2024-02-21 18:54:37 -0600

    Update custom-css-function.php

--- custom-css-function.php
+++ custom-css-function.php
@@ -5,4 +5,12 @@ function my_style_template(){
 }
 add_action( 'wp_enqueue_scripts', 'my_style_template',5 );
 //End MyStyle Function
+
+//Font Awesome Icons Function
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+
+function enqueue_load_fa() {
+wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+}
+
 ?>
